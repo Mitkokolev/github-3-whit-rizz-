e86d2419e8f7e9d7d0c93740f8c50c1ed368b313
student <student@DESKTOP-ESS347R>
2022-12-13 17:46:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿static void Main(string[] args)
+{
+    int x = int.Parse(Console.ReadLine());
+    int y = int.Parse(Console.ReadLine());
+    int Sum = int.Parse(Console.ReadLine());
+    int Substract = int.Parse(Console.ReadLine());
+
+    int sum = Sum(x, y);
+    Console.WriteLine("sum is: " + sum);
+
+    int substract = Substract(x, y);
+    Console.WriteLine("Substract is " + substract);
+}
